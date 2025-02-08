c0fcc32240e1d26fe4261fc464ed2f0a97237563
Matan Yemini <matan@engageli.com>
2025-02-08 23:15:21 +0200

    add call route

---
+++ src/routes/call_routes.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Request, WebSocket
+from fastapi.responses import HTMLResponse, JSONResponse
+from twilio.twiml.voice_response import VoiceResponse, Connect
+
+from services.openai_service import handle_media_stream
+
+router = APIRouter()
+
+@router.get("/", response_class=JSONResponse)
+async def index_page():
+    return {"message": "Twilio Media Stream Server is running!"}
+
+@router.api_route("/incoming-call", methods=["GET", "POST"])
+async def handle_incoming_call(request: Request):
+    """Handle incoming call and return TwiML response to connect to Media Stream."""
+    response = VoiceResponse()
+    response.pause(length=1)
+    host = request.url.hostname
+    connect = Connect()
+    connect.stream(url=f'wss://{host}/media-stream')
+    response.append(connect)
+    return HTMLResponse(content=str(response), media_type="application/xml")
+
+@router.websocket("/media-stream")
+async def media_stream_endpoint(websocket: WebSocket):
+    await handle_media_stream(websocket) 
